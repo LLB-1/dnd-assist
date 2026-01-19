2365bff14e4fba9d66a2acda06f5a1c062b4c2c3
Leonard <leonardbester@protonmail.com>
2026-01-19 21:56:20 +0200

    adjusted styling.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
 
     <div class="flex flex-col items-center justify-center gap-8 fixed inset-0 pointer-events-none pt-20">
         <div class="mb-8 pointer-events-auto">
-            <h1 class="text-4xl font-bold mb-4 text-center text-white">Who shall we bring to life?</h1>
+            <h1 class="text-4xl font-bold mb-4 text-accent text-">Who shall we bring to life?</h1>
         </div>
         <div class="w-1/2 max-w-2xl pointer-events-auto">
             <input type="text" placeholder="Who shall we bring to life?"
-                class="w-full px-8 py-6 text-xl rounded-full bg-white shadow-xl border-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-800" />
+                class="w-full px-8 py-6 text-xl rounded-full bg-accent shadow-xl border-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-800" />
         </div>
     </div>
 
